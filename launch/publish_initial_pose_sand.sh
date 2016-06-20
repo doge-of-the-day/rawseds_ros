a7a606910cde8f752b6395c0fc00d645c361c79f
Richard Hanten <Richard.Hanten@uni-tuebingen.de>
2016-06-20 22:57:53 +0200

    another script

---
+++ launch/publish_initial_pose_sand.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+rostopic pub /initialpose geometry_msgs/PoseWithCovarianceStamped "header: 
+  seq: 14
+  stamp: 
+    secs: 0
+    nsecs: 0
+  frame_id: map
+pose: 
+  pose: 
+    position: 
+      x: 41.578
+      y: 17.046
+      z: 0.0
+    orientation: 
+      x: 0.0
+      y: 0.0
+      z: 1.0
+      w: 0.0
+  covariance: [0.05, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.05, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.01]" -1
