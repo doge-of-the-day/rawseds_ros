b66152dd7e24709132dc0673723c4b054e06a220
Richard Hanten <richard.hanten@uni-tuebingen.de>
2017-06-27 17:26:28 +0200

    removed trashy output

--- src/tune_stereo_matching.cpp
+++ src/tune_stereo_matching.cpp
@@ -93,7 +93,6 @@ int main(int argc, char *argv[])
             } else {
                 std::cerr << id << " : paths missing" << std::endl;
             }
-            std::cout << "update" << std::endl;
         }
 
         if(!right.empty() && !left.empty()) {
