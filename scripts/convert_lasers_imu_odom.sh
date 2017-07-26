ee211703afbe4263a7d8b5f918f2f3165a5e9628
Richard Hanten <richard.hanten@uni-tuebingen.de>
2017-07-26 14:10:53 +0200

    conversion script

---
+++ scripts/convert_lasers_imu_odom.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+rosrun rawseeds_ros odometry2bag.py *ODOMETRY_XYT.csv odom.bag
+rosrun rawseeds_ros gtextodom2bag.py --frame_id /odom --topic /odom *GROUNDTRUTH.csv odom_gt.bag
+rosrun rawseeds_ros imu2bag.py --frame_id /imu --topic /rawseeds_imu *IMU_STRETCHED.csv imu.bag
+rosrun rawseeds_ros sick2bag.py --frame_id /sick_rear --topic /sick/rear *SICK_REAR.csv sick_rear.bag
+rosrun rawseeds_ros sick2bag.py --frame_id /sick_front --topic /sick/front *SICK_REAR.csv sick_front.bag
+rosrun rawseeds_ros hokuyo2bag.py --frame_id /hokyuo_rear --topic /hokuyo/rear *HOKUYO_REAR.csv hokuyo_rear.bag
+rosrun rawseeds_ros hokuyo2bag.py --frame_id /hokyuo_front --topic /hokuyo/front *HOKUYO_FRONT.csv hokuyo_front.bag
+
