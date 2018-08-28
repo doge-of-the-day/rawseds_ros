91ab38b10056f9149de57a61ae011d9cc806f8e5
Richard Hanten <richard.hanten@uni-tuebingen.de>
2018-08-28 11:43:21 +0200

    bugs

--- scripts/trajectory2relations.py
+++ scripts/trajectory2relations.py
@@ -1,18 +1,12 @@
 #!/usr/bin/python
 import argparse
-import numpy
-import rosbag
-import rospy
-import geometry_msgs.msg
-import nav_msgs.msg
-import tf
-import tf.msg
-import sys
+import numpy as np
+import sys 
 
 def main():
     parser = argparse.ArgumentParser(description='convert rawseeds .csv trajectory to relations')
-    parser.add_argument('input', help='input trajectory file')
-    parser.add_argument('output', help='output relations file')
+    parser.add_argument('--input', help='input trajectory file')
+    parser.add_argument('--output', help='output relations file')
  
     args = parser.parse_args()
 
@@ -20,7 +14,7 @@ def main():
     print('Output file: ' + args.output)
  
     in_file = open(args.input, 'r')
-    out_file = open(arg.output, 'w')
+    out_file = open(args.output, 'w')
 
     last_t  = 0.0;
     last_tf = np.identity(3);
@@ -28,40 +22,41 @@ def main():
     count = 0
 
     for l in in_file:
-        row = line.split(',')
+        row = l.split(',')
 
         t  = float(row[0])  # timestamp in SECONDS
         x  = float(row[1])
         y  = float(row[2])
         th = float(row[3])
 
-        tf      = numpy.identiy(3)
-        tf[0,0] =  numpy.cos(th)
-        tf[0,1] = -numpy.sin(th)
-        tf[1,0] =  numpy.sin(th)
-        tf[1,1] =  numpy.cos(th)
+        tf      =  np.identity(3)
+        tf[0,0] =  np.cos(th)
+        tf[0,1] = -np.sin(th)
+        tf[1,0] =  np.sin(th)
+        tf[1,1] =  np.cos(th)
         tf[0,2] =  x
         tf[1,2] =  y
 
 
 
         if last_t is not 0.0:
-            tf_rel = last_tf.inverse() * tf
+            tf_rel = np.dot(np.linalg.inv(last_tf),tf)
 
             # write out
-            out = str(last_t) + "," + 
-                  str(t) + "," + 
-                  str(tf_rel[0,2]) + "," +
-                  str(tf_rel[1,2]) + "," +
-                  str(0.0) + ","
-                  str(0.0) + ","
-                  str(0.0) + ","
-                  str(numpy.acos(tf_rel[0,0]))
+            out = str(last_t) + "," + \
+                  str(t) + "," +  \
+                  str(tf_rel[0,2]) + "," + \
+                  str(tf_rel[1,2]) + "," + \
+                  str(0.0) + "," + \
+                  str(0.0) + "," + \
+                  str(0.0) + "," + \
+                  str(np.arccos(tf_rel[0,0])) + "\n"
             out_file.write(out)
             count += 1
 
         last_tf = tf
         last_t = t
+
 
 
     print("Calculated " + str(count) + " relative transformations.")
